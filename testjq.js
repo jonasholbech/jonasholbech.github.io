6cc4178dd068f18378f3a7d99bf250e1cb88ea8f
Jonas Holbech <jh@jonasholbech.dk>
2015-12-31 00:09:30 +0100

    removed some whitespace

--- testjq.js
+++ testjq.js
@@ -1,10 +1,8 @@
 // Make sure it is public or set to Anyone with link can view
 var url = "https://spreadsheets.google.com/feeds/list/1POwYekndHckpMTTbPQccqtJhvQ3pgjPTjaLhDfumr9Y/od6/public/values?alt=json";
-
 $.getJSON(url, function(data) {
     console.log(data)
     var entry = data.feed.entry;
-
     $(entry).each(function(){
         // Column names are the first column in the spreadsheet.
         //if the column title is "age" we reference it's content with this.gsx$age.$t
